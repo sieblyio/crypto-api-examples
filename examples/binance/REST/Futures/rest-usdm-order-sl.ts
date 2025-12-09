18a6e232b0f906d2e55868c1d37873df609af990
JJ-Cro <bryanadams3312@gmail.com>
2025-12-09 16:18:53 +0100

    fix(): update stop loss order submission to use new API method and adjust price calculation

--- examples/binance/REST/Futures/rest-usdm-order-sl.ts
+++ examples/binance/REST/Futures/rest-usdm-order-sl.ts
@@ -54,14 +54,15 @@ async function start() {
         const { markPrice }: any = long;
 
         // creating SL order
-        const result = await client.submitNewOrder({
+        const result = await client.submitNewAlgoOrder({
           symbol,
+          algoType: 'CONDITIONAL',
           side: 'SELL', // the action of order, means this order will sell which is sl for long position
           positionSide: 'LONG', // based on the headge mode we either LONG or SHORT, here we are doing it for our long pos
           timeInForce: 'GTC',
           type: 'STOP_MARKET',
+          price: parseFloat((markPrice * 0.99).toFixed(3)), // set sl price 1% below current price
           closePosition: 'true', // this is here because we don't have the position quantity value, and it means closee all quantity
-          stopPrice: parseFloat((markPrice * 0.99).toFixed(3)), // set sl price 1% below current price
           workingType: 'MARK_PRICE',
         });
         console.log('SL Modifiled sell result: ', result);
