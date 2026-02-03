361bb3dd86dfc5b9bf5f297cd9b8ab75c1ab3aee
JJ-Cro <bryanadams3312@gmail.com>
2026-02-03 13:32:48 +0100

    feat(): add comprehensive examples for OKX API, including REST and WebSocket implementations in TypeScript

---
+++ examples/OKX2/Rest/rest-private-trade-market.ts
@@ -0,0 +1,115 @@
+import { OrderRequest, RestClient } from 'okx-api';
+
+// read from environmental variables
+const API_KEY = process.env.API_KEY_COM;
+const API_SECRET = process.env.API_SECRET_COM;
+const API_PASS = process.env.API_PASSPHRASE_COM;
+
+// If running from CLI in unix, you can pass env vars as such:
+// API_KEY_COM='lkm12n3-2ba3-1mxf-fn13-lkm12n3a' API_SECRET_COM='035B2B9637E1BDFFEE2646BFBDDB8CE4' API_PASSPHRASE_COM='ComplexPa$$!23$5^' ts-node examples/rest-private-trade-market.ts
+
+// note the single quotes, preventing special characters such as $ from being incorrectly passed
+
+if (!API_KEY || !API_SECRET || !API_PASS) {
+  throw new Error(
+    'Missing api credentials. Use environmental variables or hard code in the script',
+  );
+}
+
+console.log(new Date(), 'Using credentials: ', {
+  API_KEY,
+  API_SECRET,
+  API_PASS,
+});
+
+const client = new RestClient({
+  apiKey: API_KEY,
+  // apiKey: 'apiKeyHere',
+  apiSecret: API_SECRET,
+  // apiSecret: 'apiSecretHere',
+  apiPass: API_PASS,
+  // apiPass: 'apiPassHere',
+});
+
+/** Get available balance for an asset */
+async function getAssetBalance(
+  client: RestClient,
+  coin: string,
+): Promise<number | null> {
+  const allBalances = await client.getBalance();
+  const usdtBalanceResult = allBalances[0].details.find(
+    (bal) => bal.ccy === coin,
+  );
+
+  const usdtBalance = Number(usdtBalanceResult?.availBal);
+  if (!usdtBalanceResult || !usdtBalance || isNaN(usdtBalance)) {
+    return null;
+  }
+
+  return usdtBalance;
+}
+
+/**
+ * This is a simple REST API demo script wrapped in a immediately invoked function expression. The logic below will immediately execute if you run this script.
+ *
+ * It is designed to:
+ * - check for any available USDT balance
+ * - immediately use half the balance to buy bitcoin (in spot)
+ * - check the available BTC balance
+ * - immediately sell all available BTC back to USDT
+ */
+(async () => {
+  try {
+    const usdtBalance = await getAssetBalance(client, 'USDT');
+    if (!usdtBalance) {
+      console.error('No USDT to trade');
+      return;
+    }
+
+    console.log(`USDT available: ${usdtBalance}`);
+
+    const symbol = 'BTC-USDT';
+    const percentBalanceToUse = 50;
+
+    const quantity = usdtBalance * (percentBalanceToUse / 100);
+    const buyOrder: OrderRequest = {
+      instId: symbol,
+      ordType: 'market',
+      side: 'buy',
+      sz: String(quantity),
+      tdMode: 'cash',
+      tgtCcy: 'base_ccy',
+    };
+
+    console.log('submitting buy order:', buyOrder);
+    const buyResult = await client.submitOrder(buyOrder);
+
+    console.log('buy order result: ', buyResult, '\n\n');
+
+    const btcBalance = await getAssetBalance(client, 'BTC');
+    if (!btcBalance) {
+      console.error('No BTC to trade');
+      return;
+    }
+
+    console.log(`BTC available: ${btcBalance}`);
+    const sellOrder: OrderRequest = {
+      instId: symbol,
+      ordType: 'market',
+      side: 'sell',
+      sz: String(btcBalance),
+      tdMode: 'cash',
+      tgtCcy: 'base_ccy',
+    };
+
+    console.log('submitting sell order:', sellOrder);
+    const sellResult = await client.submitOrder(sellOrder);
+
+    console.log('sell order result: ', sellResult, '\n\n');
+    const usdtBalanceFinal = await getAssetBalance(client, 'USDT');
+
+    console.log('final balance: ', usdtBalanceFinal);
+  } catch (e) {
+    console.error('request failed: ', e);
+  }
+})();
---
+++ examples/OKX2/Rest/rest-private-trade.ts
@@ -0,0 +1,140 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { RestClient } from 'okx-api';
+import { OrderRequest } from 'okx-api';
+
+// read from environmental variables
+const API_KEY = process.env.API_KEY_COM;
+const API_SECRET = process.env.API_SECRET_COM;
+const API_PASS = process.env.API_PASS_COM;
+
+// If running from CLI in unix, you can pass env vars as such:
+// API_KEY_COM='lkm12n3-2ba3-1mxf-fn13-lkm12n3a' API_SECRET_COM='035B2B9637E1BDFFEE2646BFBDDB8CE4' API_PASSPHRASE_COM='ComplexPa$$!23$5^' ts-node examples/rest-private-trade.ts
+
+// note the single quotes, preventing special characters such as $ from being incorrectly passed
+
+if (!API_KEY || !API_SECRET || !API_PASS) {
+  throw new Error(
+    'Missing api credentials. Use environmental variables or hard code in the script',
+  );
+}
+
+console.log(new Date(), 'Using credentials: ', {
+  API_KEY,
+  API_SECRET,
+  API_PASS,
+});
+
+const client = new RestClient({
+  apiKey: API_KEY,
+  // apiKey: 'apiKeyHere',
+  apiSecret: API_SECRET,
+  // apiSecret: 'apiSecretHere',
+  apiPass: API_PASS,
+  // apiPass: 'apiPassHere',
+});
+
+// const wsClient = new WebsocketClient({
+//   apiKey: API_KEY,
+//   apiSecret: API_SECRET,
+//   apiPass: API_PASS,
+// });
+
+function logWSEvent(type: string, data: any) {
+  console.log(new Date(), `WS ${type} event: `, data);
+}
+
+// simple sleep function
+function promiseSleep(milliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+// WARNING: for sensitive math you should be using a library such as decimal.js!
+function roundDown(value: any, decimals: number) {
+  return Number(
+    Math.floor(parseFloat(value + 'e' + decimals)) + 'e-' + decimals,
+  );
+}
+
+/** This is a simple script wrapped in a immediately invoked function expression, designed to check for any available BTC balance and immediately sell the full amount for USDT */
+(async () => {
+  try {
+    // // Add event listeners to log websocket events on account
+    // wsClient.on('update', (data) => logWSEvent('update', data));
+    // wsClient.on('open', (data) => logWSEvent('open', data));
+    // wsClient.on('response', (data) => logWSEvent('response', data));
+    // wsClient.on('reconnect', (data) => logWSEvent('reconnect', data));
+    // wsClient.on('reconnected', (data) => logWSEvent('reconnected', data));
+    // wsClient.on('authenticated', (data) => logWSEvent('authenticated', data));
+    // wsClient.on('exception', (data) => logWSEvent('exception', data));
+
+    // // Subscribe to private account topics
+    // wsClient.subscribeTopic('SPBL', 'account');
+    // wsClient.subscribeTopic('SPBL', 'orders');
+
+    // wait briefly for ws to be ready (could also use the response or authenticated events, to make sure topics are subscribed to before starting)
+    await promiseSleep(2.5 * 1000);
+
+    const allBalances = await client.getBalance();
+    // const balances = allBalances.filter((bal) => Number(bal.available) != 0);
+    const usdtBalanceResult = allBalances[0].details.find(
+      (bal) => bal.ccy === 'USDT',
+    );
+    console.log('BTC balance result: ', usdtBalanceResult);
+
+    const usdtBalance = Number(usdtBalanceResult?.availBal);
+    // console.log('balance: ', JSON.stringify(balances, null, 2));
+
+    if (!usdtBalanceResult || !usdtBalance) {
+      console.error('No USDT to trade');
+      return;
+    }
+
+    console.log(`USDT available: ${usdtBalance}`);
+
+    const symbol = 'BTC-USDT';
+    const quantity = 0.002;
+
+    const order: OrderRequest = {
+      instId: symbol,
+      ordType: 'market',
+      side: 'buy',
+      sz: String(usdtBalance * 0.5),
+      tdMode: 'cash',
+      tgtCcy: 'base_ccy',
+    };
+
+    const buyResult = await client.submitOrder(order);
+
+    console.log('result: ', buyResult);
+    return;
+    // example to find minimum allowed size for a symbol and place an order with it
+    /*  const symbol = 'BTC-USDT-SWAP';
+
+    const symbolsResult = await client.getInstruments({
+      instType: 'SWAP',
+    });
+    const btcRules = symbolsResult.find((rule) => rule.instId === symbol);
+    console.log('btc trading rules: ', btcRules);
+    if (!btcRules) {
+      return console.log('no rules found for trading ' + symbol);
+    }
+
+    const minSize = Number(btcRules.minSz);
+
+    const order = {
+      instId: symbol,
+      tdMode: 'cross',
+      ordType: 'market',
+      side: 'sell',
+      sz: String(minSize),
+    } as const;
+
+    console.log('submitting order: ', order);
+
+    const sellResult = await client.submitOrder(order);
+
+    console.log('sell result: ', sellResult); */
+  } catch (e) {
+    console.error('request failed: ', e);
+  }
+})();
